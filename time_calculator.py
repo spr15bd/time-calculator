fe88efcf0f3f5f1e826f692ac6982ce939caee54
spr15bd <bevandady@gmail.com>
2021-01-08 01:10:56 +0000

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -4,8 +4,17 @@ def add_time(start, duration, dayOfWeek=""):
   minutes = int(start[1].split()[0])
   amOrPm = start[1].split()[1] 
 
-  total_minutes = hours * 60 + minutes 
-  new_time = total_minutes
+  total_start_minutes = hours * 60 + minutes 
 
+  duration = duration.split(":")
+  hours = int(duration[0])
+  minutes = int(duration[1])
+  print(duration)
+ 
 
+  total_minutes_to_add = hours * 60 + minutes 
+  
+
+  new_time_in_minutes = total_start_minutes + total_minutes_to_add
+  new_time = str(new_time_in_minutes / 60) + ":" + str(new_time_in_minutes % 60)
   return new_time
